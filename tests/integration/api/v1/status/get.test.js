2ff8cfabeb69ced5faac51b19a7ead502cbdc0cb
viniciuspariz <66400197+viniciuspariz@users.noreply.github.com>
2026-08-29 02:52:11 +0000

    finish `/api/v1/status` fendpoint

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,23 @@
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
+  expect(responseBody.dependencies.database.opened_connections).toEqual(1);
 });
+
+// test.only("Teste de SQL Injection", async () => {
+//   // await fetch("http://localhost:3000/api/v1/status?databaseName=local_db");
+//   // await fetch("http://localhost:3000/api/v1/status?databaseName=");
+//   // await fetch("http://localhost:3000/api/v1/status?databaseName=';");
+//   // await fetch("http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4);");
+//   await fetch(
+//     "http://localhost:3000/api/v1/status?databaseName='; SELECT pg_sleep(4); --",
+//   );
+// });
